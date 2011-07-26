9bc509f09692945e598cf24e5d397c2be1a46396
Yannick Assogba <yassogba@gmail.com>
2011-07-26 00:14:52 -0400

    tail recursive fibonnaci function. trying out loop-recur.

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -39,4 +39,19 @@
 (collection-type-cond {:one 1, :two 2, :three 3})
 (collection-type-cond "huh")
 
+;standard stack consuming fib
 
+(defn fib [n]
+  (cond
+    (= 0 n) 0
+    (= 1 n) 1
+    :else (+ (fib (- n 1)) (fib (- n 2)))))
+
+;tail recursive fib using loop-recur
+;loop-recur allow you to rebind variables each
+;pass through. According to the docs it is the only non stack-consuming
+;looping method in closure.
+(defn fib-tail [n]
+  (loop [num n, a1 0, a2 1]
+    (if (= 0 num) a1
+        (recur (- num 1), a2, (+ a1 a2)))))
