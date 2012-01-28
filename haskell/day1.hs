aef07b6573cb0c5350a197732fb62db03765d910
Yannick Assogba <yassogba@gmail.com>
2012-01-28 02:18:02 -0500

    a solution to q5: map coloring problem in haskell

--- haskell/day1.hs
+++ haskell/day1.hs
@@ -70,4 +70,32 @@ module Main where
   -- http://en.wikipedia.org/wiki/Graph_coloring
   -- http://en.wikipedia.org/wiki/Four_color_theorem
   
+  -- Color 5 states (Alabama, Mississippi, Georgia, Tennessee, Florida) with three colors (red, green ,blue)
+  -- so that no neighboring states have the same color
   
+  states = ["Alabama", "Mississippi", "Georgia", "Tennessee", "Florida"]
+  colors = ["red", "green", "blue"]
+  --, (state2, col2)
+  colorings = [[(state1, col1), (state2, col2), (state3, col3), (state4, col4), (state5, col5)] | 
+    state1 <- states, col1 <- colors,
+    state2 <- states, col2 <- colors,
+    state3 <- states, col3 <- colors,
+    state4 <- states, col4 <- colors,
+    state5 <- states, col5 <- colors,
+    state1 == "Alabama",
+    state2 == "Mississippi",
+    state3 == "Georgia",
+    state4 == "Tennessee",
+    state5 == "Florida",
+    -- rules for alabama
+    col1 /= col2,
+    col1 /= col3,
+    col1 /= col4,
+    col1 /= col5,
+    -- rules for missisippi
+    col2 /= col5,
+    -- rules for georgia
+    col3 /= col4,
+    col3 /= col5
+    -- rules for other two states are already covered
+    ]
